dde7ef571d9383c41073acf7a7a0aa078b42de11
Naadson da Costa Rebouças <SEUEMAIL@GMAIL.COM>
2026-06-12 11:39:41 -0300

    Primeira versão do site para a Keit ❤️

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+
+document.getElementById("inicio").style.display="none";
+
+document.getElementById("principal").style.display="block";
+
+document.body.style.background="#ff4d6d";
+
+},4000);
